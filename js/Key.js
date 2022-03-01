1891635fd99d06badea74568a95d8fe53abc3b05
kumankoishi.py <7iggif@naver.com>
2022-03-01 23:08:20 +0900

    Added Key conponent

---
+++ js/Key.js
@@ -0,0 +1,12 @@
+import Component from './Component';
+
+export default class Key extends Component {
+  constructor(props, state, renderTarget) {
+    super(props, state, renderTarget);
+
+    /*
+      State of Key
+        - beingPressed: boolean
+    */
+  }
+};
